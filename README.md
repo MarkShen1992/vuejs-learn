17bbc27ac697f2aab34f4a2da6586241ae47a95f
Mark <18009838166@163.com>
2024-03-02 07:21:57 +0800

    vue 单文件组件。

--- README.md
+++ README.md
@@ -27,5 +27,6 @@
   - [30 分钟学会 Vue 之脚手架与组件化开发](https://www.bilibili.com/video/BV13m4y1Y7MD/?spm_id_from=333.788&vd_source=901e0b3c23d3cff6094d75383834207f)
   - [30 分钟学会 Vue 之 VueRouter&Vuex](https://www.bilibili.com/video/BV1zF411R7cR/?spm_id_from=333.788&vd_source=901e0b3c23d3cff6094d75383834207f)
   - [20 分钟学会 Vue 综合案例](https://www.bilibili.com/video/BV1714y167Sn/?spm_id_from=333.788&vd_source=901e0b3c23d3cff6094d75383834207f)
+- vue 3.0 学习视频
   - [Vue3 核心语法](https://www.bilibili.com/video/BV1Pg4y1A7pn/?spm_id_from=333.788&vd_source=901e0b3c23d3cff6094d75383834207f)
   - [Pinia](https://www.bilibili.com/video/BV1s94y1q79q/?spm_id_from=333.788&vd_source=901e0b3c23d3cff6094d75383834207f)
--- wuyou_vue_course/component/my-vue-project/src/App.vue
+++ wuyou_vue_course/component/my-vue-project/src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>父组件接收子组件的数据 {{ childData }}</h1>
+    <HelloWorld
+      msg="Welcome to Your Vue.js App"
+      :count="parentCount"
+      @child-count-change="handler"
+      >这是【插槽的位置】默认内容1
+      <!-- <template v-slot:footer>第一个footer</template> -->
+      <!-- <template #footer="childObj">第一个footer{{ childObj.childCount }}</template -->
+      <template #footer1="{ childCount }"
+        >第一个footer1{{ childCount }}
+      </template>
+    </HelloWorld>
+    <!-- 插槽 -->
+    <HelloWorld>这是【插槽的位置】默认内容2</HelloWorld>
+    <HelloWorld>这是【插槽的位置】默认内容3</HelloWorld>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 
@@ -10,9 +25,20 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      parentCount: 12,
+      childData: 0,
+    }
+  },
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  methods: {
+    handler(childCount) {
+      this.childData = childCount
+    },
+  },
 }
 </script>
 
